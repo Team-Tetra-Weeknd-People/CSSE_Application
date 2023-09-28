c733098fb95358df0341fcf39b5d2e8e98bac188
Randula Mawaththta <randula98@gmail.com>
2023-09-28 09:32:49 +0530

    Frontend Init & Struct Done

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import {
+    Navbar
+} from "../components";
+
+import {
+    Home
+} from "../pages";
+
+export default function App() {
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </Router>
+    )
+}
