5eb2dce2fd44c3e1cbb56d28a53e5698dfc9b530
Randula Mawaththta <randula98@gmail.com>
2023-10-15 00:02:05 +0530

    Grid Added

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import {
-    Home,
-    ProcStaffLogin,
-    SelectUser,
+  Home,
+  ProcStaffLogin,
+  ManagerLogin,
+  SiteManagerLogin,
+  SupplierLogin,
+  Grid,
 } from './pages';
 
 const Stack = createStackNavigator();
 
 function App() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home">
-                <Stack.Screen name="ProcStaffLogin" component={ProcStaffLogin} />
-                <Stack.Screen name="SelectUser" component={SelectUser} />
-                <Stack.Screen name="Home" component={Home} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ProcStaffLogin" component={ProcStaffLogin} />
+        <Stack.Screen name="ManagerLogin" component={ManagerLogin} />
+        <Stack.Screen name="SiteManagerLogin" component={SiteManagerLogin} />
+        <Stack.Screen name="SupplierLogin" component={SupplierLogin} />
+
+        <Stack.Screen name="Grid" component={Grid} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
