2a11958702d0520c7727b370a10c217c59eb7932
Randula Mawaththta <randula98@gmail.com>
2023-10-15 10:31:30 +0530

    Service Endpoints Added

---
+++ mobile/services/Admin.Service.jsx
@@ -0,0 +1,59 @@
+import axios from "axios";
+import * as url from './constants/url.jsx'
+
+export const register = async (data) => {
+    return await axios.post(url.admin, data, {
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+}
+
+export const login = async (data) => {
+    return await axios.post(url.adminAuth, data, {
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+}
+
+export const getAllAdmins = async () => {
+    return await axios.get(url.admin, {
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+}
+
+export const getAdmin = async (id) => {
+    return await axios.get(url.adminID(id), {
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+}
+
+export const updateAdmin = async (id, data) => {
+    return await axios.put(url.adminID(id), data, {
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+}
+
+export const deleteAdmin = async (id) => {
+    return await axios.delete(url.adminID(id), {
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+}
+
+export default {
+    register,
+    login,
+    getAllAdmins,
+    getAdmin,
+    updateAdmin,
+    deleteAdmin,
+}
---
+++ mobile/services/Site.Service.jsx
@@ -0,0 +1,59 @@
+import axios from "axios";
+import * as url from './constants/url.jsx'
+
+export const getAllSite = async () => {
+    return await axios.get(url.site, {
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+}
+
+export const getOneSite = async (id) => {
+    return await axios.get(url.siteGetOne(id), {
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+}
+
+export const getSiteByManager = async (id) => {
+    return await axios.get(url.siteSiteManager(id), {
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+}
+
+export const createSite = async (data) => {
+    return await axios.post(url.site, data, {
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+}
+
+export const updateSite = async (id, data) => {
+    return await axios.put(url.siteID(id), data, {
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+}
+
+export const deleteSite = async (id) => {
+    return await axios.delete(url.siteID(id), {
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+}
+
+export default {
+    getAllSite,
+    getOneSite,
+    getSiteByManager,
+    createSite,
+    updateSite,
+    deleteSite,
+}
