14a2ca9b26f07d175c04adc7bed9196199e78a8f
Randula Mawaththta <randula98@gmail.com>
2023-10-15 14:09:15 +0530

    Mobile Login Implemented

---
+++ mobile/pages/Dashboards/SiteManagerDashboard.js
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export default function SiteManagerDashboard() {
+  return (
+    <View>
+      <Text>SiteManagerDashboard</Text>
+    </View>
+  );
+}
