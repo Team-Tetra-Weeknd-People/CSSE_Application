509137104d9fd8062b8c655a25f2cd5fb4ee2d25
SuDelk <sudul.fernando@gmail.com>
2023-10-16 23:03:23 +0530

    delivery notes small changes

--- mobile/pages/ProcurementStaff/Screens/ProcDeliveryNoteScreen.js
+++ mobile/pages/ProcurementStaff/Screens/ProcDeliveryNoteScreen.js
@@ -9,22 +9,9 @@ export default function ProcDeliveryNoteScreen({ route }) {
   return (
     <ScrollView>
     {deliveryNote ? (<View style={styles.container}>
-      <Text style={styles.heading}>Delivery Note Details</Text>
       <View style={styles.detailsContainer}>
         <Text style={styles.label}>Delivery Note ID:</Text>
         <Text style={styles.value}>{deliveryNote._id}</Text>
-      </View>
-      <View style={styles.deliveryDescriptionContainer}>
-        <Text style={styles.label}>Delivery Description:</Text>
-        <Text style={styles.deliveryDescription}>{deliveryNote.deliveryDescription}</Text>
-      </View>
-      <View style={styles.detailsContainer}>
-        <Text style={styles.label}>Delivery Note:</Text>
-        <Text style={styles.value}>{deliveryNote.deliveryNote}</Text>
-      </View>
-      <View style={styles.detailsContainer}>
-        <Text style={styles.label}>Item Description:</Text>
-        <Text style={styles.value}>{deliveryNote.itemDescription}</Text>
       </View>
       <View style={styles.detailsContainer}>
         <Text style={styles.label}>Order ID:</Text>
@@ -35,13 +22,34 @@ export default function ProcDeliveryNoteScreen({ route }) {
         <Text style={styles.value}>{deliveryNote.supplierId}</Text>
       </View>
       <View style={styles.detailsContainer}>
-        <Text style={styles.label}>Created On:</Text>
-        <Text style={styles.value}>{deliveryNote.createdOn}</Text>
+        <Text style={styles.label}>Created Date:</Text>
+        <Text style={styles.value}>{deliveryNote.createdOn.toString().split("T")[0]}</Text>
       </View>
       <View style={styles.detailsContainer}>
-        <Text style={styles.label}>Updated On:</Text>
-        <Text style={styles.value}>{deliveryNote.updatedOn}</Text>
+        <Text style={styles.label}>Created Time:</Text>
+        <Text style={styles.value}>{deliveryNote.createdOn.toString().split("T")[1].toString().split("Z")[0]}</Text>
       </View>
+      <View style={styles.detailsContainer}>
+        <Text style={styles.label}>Last Updated Date:</Text>
+        <Text style={styles.value}>{deliveryNote.updatedOn.toString().split("T")[0]}</Text>
+      </View>
+      <View style={styles.detailsContainer}>
+        <Text style={styles.label}>Last Updated Time:</Text>
+        <Text style={styles.value}>{deliveryNote.updatedOn.toString().split("T")[1].toString().split("Z")[0]}</Text>
+      </View>
+      <View style={styles.deliveryDescriptionContainer}>
+        <Text style={styles.label}>Delivery Description:</Text>
+        <Text style={styles.deliveryDescription}>{deliveryNote.deliveryDescription}</Text>
+      </View>
+      <View style={styles.deliveryDescriptionContainer}>
+        <Text style={styles.label}>Delivery Note:</Text>
+        <Text style={styles.deliveryDescription}>{deliveryNote.deliveryNote}</Text>
+      </View>
+      <View style={styles.deliveryDescriptionContainer}>
+        <Text style={styles.label}>Item Description:</Text>
+        <Text style={styles.deliveryDescription}>{deliveryNote.itemDescription}</Text>
+      </View>
+      
     </View>): <View><Text>Please Select an Order From Order Requests Tab</Text></View>}
     </ScrollView>
   );
@@ -69,11 +77,14 @@ const styles = StyleSheet.create({
   label: {
     fontSize: 16,
     fontWeight: 'bold',
+    color: 'black',
   },
   value: {
     fontSize: 16,
+    color: 'purple',
   },
   deliveryDescription: {
+    color: 'purple',
     fontSize: 16,
   },
 });
